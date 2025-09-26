e217d2405be63e608508845ddec89682e250e6fa
danielkembeu <kembeudaniel2@gmail.com>
2025-09-26 20:09:45 +0100

    [info] platform built. First version

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// Define __dirname for ESM environments (like Vite config)
+const __dirname = path.resolve();
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
